1503bdc15ebbf83e3f112112eabd80b32e857989
Rohan <rohan.seth497@gmail.com>
2018-10-02 15:24:51 +0530

    changes

--- app/src/main/java/com/zorail/powermanager/CreateAccount/CreateAccountPresenter.java
+++ app/src/main/java/com/zorail/powermanager/CreateAccount/CreateAccountPresenter.java
@@ -1,6 +1,5 @@
 package com.zorail.powermanager.CreateAccount;
 
-import android.content.Intent;
 import android.support.annotation.NonNull;
 
 import com.google.android.gms.tasks.OnCompleteListener;
