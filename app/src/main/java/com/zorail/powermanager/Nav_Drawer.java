bc3f5d9a996ef06ed9c44de4201f75814a6473b4
Rohan <rohan.seth497@gmail.com>
2018-10-02 14:31:51 +0530

    configured ssh key

--- app/src/main/java/com/zorail/powermanager/Nav_Drawer.java
+++ app/src/main/java/com/zorail/powermanager/Nav_Drawer.java
@@ -62,9 +62,7 @@ public class Nav_Drawer extends AppCompatActivity
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         displaySelectedScreen(item.getItemId());
 
         return true;
